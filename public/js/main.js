6a8e24dcffcdf7bfc009de75c437b9c8b09ab8ba
Bryson Coons <98131408+tacbodell@users.noreply.github.com>
2024-05-04 15:24:49 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,29 @@
+// set constants to access DOM elements
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// add event listeners to every delete button
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
 
+// add event listeners to every incomplete todo item
 Array.from(item).forEach((element)=>{
     element.addEventListener('click', markComplete)
 })
 
+// add event listeners to every completed todo item
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// deletes 1 item clicked on from database
 async function deleteItem(){
+    // grab name of todo item
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // send request to delete item by passing itemText into body of JSON request
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -24,6 +31,7 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+        // log that request is finished and refresh page
         const data = await response.json()
         console.log(data)
         location.reload()
@@ -33,9 +41,12 @@ async function deleteItem(){
     }
 }
 
+// updates 1 item clicked on as complete
 async function markComplete(){
+    // grab name of todo item
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // send request to update item by passing itemText into body of JSON request
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -44,6 +55,7 @@ async function markComplete(){
             })
           })
         const data = await response.json()
+        // log that request is finished and refresh page
         console.log(data)
         location.reload()
 
@@ -52,9 +64,11 @@ async function markComplete(){
     }
 }
 
+// updates 1 item clicked on as uncomplete
 async function markUnComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        // send request to update item by passing itemText into body of JSON request
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -63,6 +77,7 @@ async function markUnComplete(){
             })
           })
         const data = await response.json()
+        // log that request is finished and refresh page
         console.log(data)
         location.reload()
 
